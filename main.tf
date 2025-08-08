b593cfa935eeb9efde83ede3b73a31730bb7e980
Paul Muller <39515127+pjrellum@users.noreply.github.com>
2025-08-08 06:02:58 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,8 @@
+# In locals voor environment-specifieke configuratie
+locals {
+  storage_tier = var.environment == "prd" ? "Premium" : "Standard"
+}
+
 # Random suffix om storage account naam uniek te maken
 resource "random_id" "suffix" {
   byte_length = 4
@@ -5,17 +10,17 @@ resource "random_id" "suffix" {
 
 # Resource Group
 resource "azurerm_resource_group" "workshop" {
-  name     = "rg-terraform-workshop-${var.student_name}"
+  name     = "rg-terraform-workshop-${var.student_name}-${var.environment}"
   location = var.location
   tags     = var.tags
 }
 
 # Storage Account
 resource "azurerm_storage_account" "workshop" {
-  name                     = "st${var.student_name}${random_id.suffix.hex}"
+  name                     = "st${var.student_name}${var.environment}${random_id.suffix.hex}"
   resource_group_name      = azurerm_resource_group.workshop.name
   location                 = azurerm_resource_group.workshop.location
-  account_tier             = "Standard"
+  account_tier             = local.storage_tier
   account_replication_type = "LRS"
   
   # Minimale configuratie voor workshop
